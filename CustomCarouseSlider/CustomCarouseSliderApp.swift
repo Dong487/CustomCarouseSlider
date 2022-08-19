328ba0aa71b1f222fe5111ac63aab8664537f78b
Dong487 <Dsheng7777777@gmail.com>
2022-08-20 01:09:34 +0800

    Initial Commit

---
+++ CustomCarouseSlider/CustomCarouseSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomCarouseSliderApp.swift
+//  CustomCarouseSlider
+//
+//  Created by DONG SHENG on 2022/8/20.
+//
+
+import SwiftUI
+
+@main
+struct CustomCarouseSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
